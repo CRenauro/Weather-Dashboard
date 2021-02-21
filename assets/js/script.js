6edda18fb530d66fc0a35486d40bdb7f51d39bda
clara_r <crenauro@optimum.net>
2021-02-21 14:44:04 -0500

    layout done, updated html file

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var token = config.MY_API_TOKEN;
+var key = config.SECRET_API_KEY;
+
+https://www.your-api-host.com/?query&id=' + token + '&pass=' + key
+
+api.openweathermap.org/data/2.5/weather?q={city name}
